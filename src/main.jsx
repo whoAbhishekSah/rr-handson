1cb135a54ec29dd3a6f17953fd54f55b0d2994cf
Abhishek Sah <abhi.sah.97@gmail.com>
2024-05-17 11:43:39 +0530

    add external router components

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import Root from './routes/root'
+import './index.css'
 import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 
-const router = createBrowserRouter([{path: "/", element: <div>Hello world</div>}])
+const router = createBrowserRouter([{path: "/", element: <Root />}])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
