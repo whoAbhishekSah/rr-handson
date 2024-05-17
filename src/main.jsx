0f7811ecadaff444d4894bd0d94d3d6b61daa27a
Abhishek Sah <abhi.sah.97@gmail.com>
2024-05-17 12:18:08 +0530

    make contacts part of root route

--- src/main.jsx
+++ src/main.jsx
@@ -7,9 +7,17 @@ import './index.css'
 import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 
 const router = createBrowserRouter([
-  {path: "/", element: <Root />, errorElement:<ErrorPage />},
-  {path: "/contacts/:contactId", element: <Contact />},
-
+  {
+    path: "/", 
+    element: <Root />, 
+    errorElement:<ErrorPage />, 
+    children:[
+      {
+        path: "contacts/:contactId", 
+        element: <Contact />
+      }
+    ]
+  }
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
