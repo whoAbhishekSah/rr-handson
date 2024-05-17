26b71140b56be9dc0543640a7addb4a1fcaf02fd
Abhishek Sah <abhi.sah.97@gmail.com>
2024-05-17 12:03:32 +0530

    add dynamic contacts route

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Root from './routes/root'
 import ErrorPage from './error-page'
+import Contact from './routes/contacts'
 import './index.css'
 import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 
-const router = createBrowserRouter([{path: "/", element: <Root />, errorElement:<ErrorPage />}])
+const router = createBrowserRouter([
+  {path: "/", element: <Root />, errorElement:<ErrorPage />},
+  {path: "/contacts/:contactId", element: <Contact />},
+
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
