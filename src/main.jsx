af73c4e64481141415732323f60dfe9bdb490e0e
Abhishek Sah <abhi.sah.97@gmail.com>
2024-05-17 11:47:33 +0530

    add error page components

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Root from './routes/root'
+import ErrorPage from './error-page'
 import './index.css'
 import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 
-const router = createBrowserRouter([{path: "/", element: <Root />}])
+const router = createBrowserRouter([{path: "/", element: <Root />, errorElement:<ErrorPage />}])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
